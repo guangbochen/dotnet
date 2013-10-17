8c0f8b0a0e02c929113d3fb0dd228d95a8ff91b3
unknown <steven.guangbo.chen@gmail.com>
2013-10-17 21:16:30 +1100

    complete net in form Application

---
+++ Ass2/Ass2/Ass2/Battle.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass2
+{
+    public class Battle
+    {
+        private int round = 0;
+        public Battle(Fleet fleet1, Fleet fleet2, Random rand, Ass2Form form)
+        {
+            while (!fleet1.lostTheBattel() && !fleet2.lostTheBattel())
+            {
+                round++;
+
+                //reset each ships as not target in each new round 
+                fleet1.fireWeapon(fleet2, rand);
+                fleet2.fireWeapon(fleet1, rand);
+
+                //remove the destroyed ships
+                fleet1.removeDestroyedShips(round, form);
+                fleet2.removeDestroyedShips(round, form);
+
+                //regenerate the shield strength after each round
+                fleet1.regenerateShield();
+                fleet2.regenerateShield();
+            }
+
+            //print out the battle result
+            PrintResult(fleet1, fleet2, round, form);
+        }
+
+        /**
+         * this method print out the battle result
+         **/
+        private void PrintResult(Fleet fleet1, Fleet fleet2, int round, Ass2Form form)
+        {
+            if (!fleet1.lostTheBattel())
+            {
+                //fleet 1 win the battle
+                getDamageReport(fleet1, fleet2, form);
+            }
+            else if (!fleet2.lostTheBattel())
+            {
+                //fleet 2 win the battle
+                getDamageReport(fleet2, fleet1, form);
+            }
+            else
+            {
+                //print battle result as a draw
+                form.addTextToResult("After round " + round + " the battle has been a " +
+                    "draw with both sides destroyed");
+            }
+        }
+
+        /**
+         * this method calculates the level of damage
+         **/
+        private void getDamageReport(Fleet fleet1, Fleet fleet2, Ass2Form form)
+        {
+            int lostShips, survivedShips;
+            Ships ships = fleet1.Ships;
+            survivedShips = ships.ShipList.Count;
+            lostShips = fleet1.NumberOfShips - survivedShips;
+            form.addTextToResult("\r\nAfter round " + round + " the " + fleet1.FleetName + " fleet won");
+            form.addTextToResult("  " + fleet2.NumberOfShips + " enemy ships destroyed");
+            form.addTextToResult("  " + lostShips + " ships lost");
+            form.addTextToResult("  " + survivedShips + " ships survived");
+            foreach (BaseShip ship in ships.ShipList)
+            {
+                Hull hull = ship.Hull;
+                form.addTextToResult("    " + ship.Name + " - " + hull.getDamageRating());
+            }
+        }
+    }
+}
---
+++ Ass2/Ass2/Ass2/Hull.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass2
+{
+    public class Hull
+    {
+        private int hullStrength;
+        private int maxHullStrength;
+        private string line;
+        private enum HullDamagePercent
+        {
+            VERY_HEAVY_DAMAGE = 0,
+            HEAVY_DAMAGE = 40,
+            MODERATE_DAMAGE = 70,
+            LIGHT_DAMAGE = 90,
+            UNDAMAGED = 100
+        }
+
+        public int HullStrength
+        {
+            get { return hullStrength; }
+        }
+
+        public void init(int hull)
+        {
+            hullStrength = maxHullStrength = hull;
+        }
+
+        public Hull()
+        {
+            hullStrength = maxHullStrength = -1;
+            line = "";
+        }
+
+        public void loadHullStre(StreamReader fin, string name)
+        {
+            //load ship hull strength
+            line = fin.ReadLine();
+            if (!Int32.TryParse(line, out hullStrength) || hullStrength < 1)
+                throw new Exception("Invalid hull strength in ship class " + name);
+            maxHullStrength = hullStrength;
+        }
+
+        public void takeDamage(int damage)
+        {
+            hullStrength -= damage;
+        }
+
+        public string getDamageRating()
+        {
+            //calculate and return damage percent
+            int undamagedPercent = (hullStrength) * 100 / maxHullStrength;
+            if (undamagedPercent == (int)HullDamagePercent.UNDAMAGED)
+                return "undamaged";
+            if (undamagedPercent >= (int)HullDamagePercent.LIGHT_DAMAGE)
+                return "lightly damaged";
+            if (undamagedPercent >= (int)HullDamagePercent.MODERATE_DAMAGE)
+                return "moderately damaged";
+            if (undamagedPercent >= (int)HullDamagePercent.HEAVY_DAMAGE)
+                return "heavily damaged";
+            return "very heavily damaged";
+        }
+    }
+}
